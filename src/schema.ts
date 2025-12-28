1955bf7f3a131e7ef9f0ee1c144ab5d9aa5a5f22
Ralica Popova <ralicaterzieva90@gmail.com>
2025-12-28 19:38:45 +0200

    Add schema types

--- src/schema.ts
+++ src/schema.ts
@@ -1,7 +1,30 @@
-import { ApolloServer } from "@apollo/server";
+
 
 export const typeDefs = `#graphql
   type Query {
     hello: String!
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    createdAt: String!
+    published: Boolean!
+    user: User!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    profile: Profile!
+    posts: [Post!]!
+  }
+
+  type Profile {
+    id: ID!
+    bio: String!
+    user: User!
   }
   `
