a683f016563beb3abe95c14bd0951ed72404b9f2
Ralica Popova <ralicaterzieva90@gmail.com>
2026-01-03 18:26:10 +0200

    Add user post relationship

---
+++ src/resolvers/User.ts
@@ -0,0 +1,36 @@
+import { MyContext } from "../context";
+
+interface UserParentType {
+  id: number;
+}
+
+export const User = {
+  posts: (parent: UserParentType, __: any, { userInfo, prisma }: MyContext) => {
+    const isOwnProfile = parent.id === userInfo?.userId;
+
+    if (isOwnProfile) {
+      return prisma.post.findMany({
+        where: {
+          authorId: parent.id,
+        },
+        orderBy: [
+          {
+            createdAt: "desc",
+          },
+        ],
+      });
+    } else {
+      return prisma.post.findMany({
+        where: {
+          authorId: parent.id,
+          published: true,
+        },
+        orderBy: [
+          {
+            createdAt: "desc",
+          },
+        ],
+      });
+    }
+  },
+};
